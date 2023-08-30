6954269e57a0f2dc0e276e89cfe570bb60ad9173
dlwhsk0 <hana8145@gmail.com>
2023-08-30 22:30:48 +0900

    1308번

--- BaekJoon/13022.cpp
+++ BaekJoon/13022.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <vector>
 using namespace std;
 
 char wolf[4] = { 0, };
 bool ans = true;
-vector<char> v;
 
 void check() { // 각 알파벳의 개수가 똑같나 확인하는 함수
     for (int i = 0; i < 4; i++) {
@@ -21,10 +19,7 @@ int main() {
     cin.tie(0); cout.tie(0);
 
     string str;
-    int n, len;
-
     cin >> str;
-    len = str.length();
 
     for (int i = 0; i < str.size(); i++) {
         if (str[i] == 'w') { wolf[0]++; }
@@ -32,28 +27,9 @@ int main() {
         else if (str[i] == 'l') { wolf[2]++; }
         else if (str[i] == 'f') {
             wolf[3]++;
-
             if (str[i + 1] == 'w') { check(); }
         }
 
-        //if (i >= 1) { // 순서가 맞는지 검사
-        //    if (str[i] == 'w') {
-        //        if (str[i - 1] == 'w' || str[i - 1] == 'f') { v.push_back(str[i]); }
-        //        else { ans = false; }
-        //    }
-        //    else if (str[i] == 'o') {
-        //        if (str[i - 1] == 'o' || str[i - 1] == 'w') { v.push_back(str[i]); }
-        //        else { ans = false; }
-        //    }
-        //    else if (str[i] == 'l') {
-        //        if (str[i - 1] == 'l' || str[i - 1] == 'o') { v.push_back(str[i]); }
-        //        else { ans = false; }
-        //    }
-        //    else if (str[i] == 'f') {
-        //        if (str[i - 1] == 'l' || str[i - 1] == 'f') { v.push_back(str[i]); }
-        //        else { ans = false; }
-        //    }
-        //}
         if (i >= 1) { // 순서가 맞는지 검사
             if (str[i] == 'w') {
                 if (!(str[i - 1] == 'w' || str[i - 1] == 'f')) { ans = false; }
