b936260a5b5689a77fc9962f42632859ea9f52af
dlwhsk0 <94193594+dlwhsk0@users.noreply.github.com>
2023-11-22 19:29:07 +0900

    [Bronze I] Title: 딱지놀이, Time: 0 ms, Memory: 2024 KB, Score: 13 point -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <map>
+#define ll long long
+using namespace std;
+
+map<int, int> a;
+map<int, int> b;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, anum, aa, bnum, bb;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        a.clear();
+        b.clear();
+
+        cin >> anum;
+        for (int j = 0; j < anum; j++) {
+            cin >> aa;
+            a[aa]++;
+        }
+
+        cin >> bnum;
+        for (int j = 0; j < anum; j++) {
+            cin >> bb;
+            b[bb]++;
+        }
+
+        bool check = 1;
+        for (int j = 4; j > 0; j--) {
+            if (a[j] > b[j]) {
+                cout << "A\n"; check = 0; break;
+            }
+            else if (a[j] < b[j]) { cout << "B\n"; check = 0; break; }
+        }
+        if (check) { cout << "D\n"; }
+    }
+    
+    return 0;
+}
